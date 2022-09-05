6c26ee50c73a907a10455abb8c5170b8c34f43bb
Sattar Rahimbayli <sattar.rahimbayli@dedalus.eu>
2022-09-05 20:32:21 +0200

    level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -10,7 +10,39 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
-
-function levelWidth(root) {}
+const Node = require('./node');
+function levelWidth(root) {
+    
+    var parent = [root];
+    var kids = [[root.data]]
+    var counter = parent.length;
+    var tmp = 0;
+    var newKids = [];
+    while(parent.length){
+        const node = parent.shift();        
+        tmp = tmp + node.children.length;
+        for(let child of node.children){
+            newKids.push(child.data);
+            parent.push(child);
+        }
+        counter = counter - 1;
+        if(counter==0){
+            kids.push(newKids);
+            counter = tmp;
+            tmp = 0;
+            newKids = [];
+        }
+        
+    }
+    var answer = []
+    while(kids.length){
+        const row = kids.shift();        
+        if(row.length){
+            answer.push(row.length)
+        }
+    }
+    
+    return answer;
+}
 
 module.exports = levelWidth;
